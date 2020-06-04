62a95a694825ddaa4da3f9e44732df55a4ebc196
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2020-06-04 13:01:12 +0300

    Updates

---
+++ precache-manifest.2c24ed4b488532a7bd51062f7ed01b42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88c087ed819d830c5eae088dc06525e",
+    "url": "/aaronkaneti.com/index.html"
+  },
+  {
+    "revision": "3623d043bb76aefa4376",
+    "url": "/aaronkaneti.com/static/css/2.cc1a181b.chunk.css"
+  },
+  {
+    "revision": "404d4de8fa8c98d5d152",
+    "url": "/aaronkaneti.com/static/css/main.70a71adc.chunk.css"
+  },
+  {
+    "revision": "3623d043bb76aefa4376",
+    "url": "/aaronkaneti.com/static/js/2.bfa1f838.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/aaronkaneti.com/static/js/2.bfa1f838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404d4de8fa8c98d5d152",
+    "url": "/aaronkaneti.com/static/js/main.ba9bd824.chunk.js"
+  },
+  {
+    "revision": "78217fe4d7316d3252af",
+    "url": "/aaronkaneti.com/static/js/runtime-main.1181fde4.js"
+  },
+  {
+    "revision": "83ad7bf7246f6d031a34b72b4b34b633",
+    "url": "/aaronkaneti.com/static/media/HearoLogo.83ad7bf7.png"
+  },
+  {
+    "revision": "ea01439157ac10390e4d44a1a7365ff6",
+    "url": "/aaronkaneti.com/static/media/IEEELogo.ea014391.png"
+  },
+  {
+    "revision": "dd04fe1b4ab7032c36de6e6cd1eea778",
+    "url": "/aaronkaneti.com/static/media/NULogo.dd04fe1b.png"
+  },
+  {
+    "revision": "85ecdfa4bc80a6eb523e91cccf409fa9",
+    "url": "/aaronkaneti.com/static/media/profile_pic.85ecdfa4.jpg"
+  },
+  {
+    "revision": "17c11462f0f8ec39626d3f30b3bfdb9d",
+    "url": "/aaronkaneti.com/static/media/resume.17c11462.pdf"
+  }
+]);
