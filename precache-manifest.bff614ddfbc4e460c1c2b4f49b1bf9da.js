7b7f38f287222fa11e06b33235b4f0b32c77319f
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2020-06-04 13:36:55 +0300

    Updates

---
+++ precache-manifest.bff614ddfbc4e460c1c2b4f49b1bf9da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7463edfbae8da2d8ff5e622cdda36e9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3623d043bb76aefa4376",
+    "url": "/static/css/2.cc1a181b.chunk.css"
+  },
+  {
+    "revision": "404d4de8fa8c98d5d152",
+    "url": "/static/css/main.70a71adc.chunk.css"
+  },
+  {
+    "revision": "3623d043bb76aefa4376",
+    "url": "/static/js/2.bfa1f838.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/static/js/2.bfa1f838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404d4de8fa8c98d5d152",
+    "url": "/static/js/main.ba9bd824.chunk.js"
+  },
+  {
+    "revision": "2544701753052af4af20",
+    "url": "/static/js/runtime-main.03b44275.js"
+  },
+  {
+    "revision": "83ad7bf7246f6d031a34b72b4b34b633",
+    "url": "/static/media/HearoLogo.83ad7bf7.png"
+  },
+  {
+    "revision": "ea01439157ac10390e4d44a1a7365ff6",
+    "url": "/static/media/IEEELogo.ea014391.png"
+  },
+  {
+    "revision": "dd04fe1b4ab7032c36de6e6cd1eea778",
+    "url": "/static/media/NULogo.dd04fe1b.png"
+  },
+  {
+    "revision": "85ecdfa4bc80a6eb523e91cccf409fa9",
+    "url": "/static/media/profile_pic.85ecdfa4.jpg"
+  },
+  {
+    "revision": "17c11462f0f8ec39626d3f30b3bfdb9d",
+    "url": "/static/media/resume.17c11462.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aaronkaneti.com/precache-manifest.2c24ed4b488532a7bd51062f7ed01b42.js"
+  "/precache-manifest.bff614ddfbc4e460c1c2b4f49b1bf9da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aaronkaneti.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
