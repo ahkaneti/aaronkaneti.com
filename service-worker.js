f2fd8f42d7c0667cf38e4ba95aa448b844b0a38a
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2020-07-20 15:59:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.854e59d4dbb97e3d33f91ade3e63af50.js"
+  "/precache-manifest.056713b38bf80b001ab68430cd96722b.js"
 );
 
 self.addEventListener('message', (event) => {
