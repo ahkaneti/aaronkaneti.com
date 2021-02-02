ef6d2562b573d0a82cc1e15f8bfd40d257920b89
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2021-02-02 13:37:29 -0500

    Resume updated change

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,48 +1,77 @@
-import React from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import resume from '../assets/download_resume.pdf';
-import Typist from 'react-typist';
-import 'react-typist/dist/Typist.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import resume from "../assets/download_resume.pdf";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import { Link } from "react-router-dom";
 
-import '../styles/App.css'
+import "../styles/App.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-
-
 function removeTextLayerOffset() {
-  const textLayers = document.querySelectorAll('.react-pdf__Page__textContent');
+  const textLayers = document.querySelectorAll(".react-pdf__Page__textContent");
   textLayers.forEach((layer) => {
     const { style } = layer;
-    style.top = '0';
-    style.left = '0';
-    style.transform = '';
+    style.top = "0";
+    style.left = "0";
+    style.transform = "";
   });
 }
 
-const Resume=(props)=>{
+const Resume = (props) => {
   const resumeHeight = window.innerHeight * 1.5;
-  return(
-    <div className={global.isMobile? "App_mobile":"App"}>
-      <div className={props.mode ? "mid light-screen":"mid"}>
-        {global.isMobile? <h1 className={props.mode? "welcome-text-mobile_main light-top-component":"welcome-text-mobile_main"}><strong>Résumé</strong></h1>: <h1><Typist><Typist.Delay ms={1000} /><strong>Résumé</strong></Typist></h1>}
-        {global.isMobile? <div/>:<h2><strong>Last Updated 20th of July, 2020.</strong></h2>}
-        <Document file={resume} className={global.isMobile? "resume_holder_mobile":"resume_holder"}>
-          <Page height={resumeHeight} style={{overflow:"hidden"}} pageNumber={1} onLoadSuccess={removeTextLayerOffset} renderAnnotationLayer />
+  return (
+    <div className={global.isMobile ? "App_mobile" : "App"}>
+      <div className={props.mode ? "mid light-screen" : "mid"}>
+        {global.isMobile ? (
+          <h1
+            className={
+              props.mode
+                ? "welcome-text-mobile_main light-top-component"
+                : "welcome-text-mobile_main"
+            }
+          >
+            <strong>Résumé</strong>
+          </h1>
+        ) : (
+          <h1>
+            <Typist>
+              <Typist.Delay ms={1000} />
+              <strong>Résumé</strong>
+            </Typist>
+          </h1>
+        )}
+        {global.isMobile ? (
+          <div />
+        ) : (
+          <h2>
+            <strong>Last Updated February 1st, 2021.</strong>
+          </h2>
+        )}
+        <Document
+          file={resume}
+          className={global.isMobile ? "resume_holder_mobile" : "resume_holder"}
+        >
+          <Page
+            height={resumeHeight}
+            style={{ overflow: "hidden" }}
+            pageNumber={1}
+            onLoadSuccess={removeTextLayerOffset}
+            renderAnnotationLayer
+          />
         </Document>
-        {global.isMobile ? 
-            <Link
-              to="/"
-              className="navitem">
-                <div className={props.mode? "route light-component":"route"}>
-                  Home
-                  </div>
-                </Link>
-          :
-          <div/>}
-      </div> 
+        {global.isMobile ? (
+          <Link to="/" className="navitem">
+            <div className={props.mode ? "route light-component" : "route"}>
+              Home
+            </div>
+          </Link>
+        ) : (
+          <div />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Resume;
